2f11e5079513a9da544e625f923bbfd6c5436119
julio1927 <jrivas6219@conestogac.on.ca>
2020-08-31 14:37:22 -0400

    added BugerBuilder container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout';
+import BugerBuilder from './containers/BugerBuilder';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <p>Test</p>
+          <BugerBuilder/>
         </Layout>
       </div>
     );
