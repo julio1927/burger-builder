c1ff91a7f9d19b0d3fd1faea816b0f8412cdab4d
julio1927 <jrivas6219@conestogac.on.ca>
2020-09-01 16:33:07 -0400

    added generic method of outputting ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -11,12 +11,19 @@ import React from 'react';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
 
-const burger = (props) => {
+const burger = (props) => { 
+
+    // Mapping object into an array of ingredients. Lets me know how many ingredients I need and what ingredients I need 
+    const transformedIngredients = Object.keys(props.ingredients).map(ingKey => {
+        return [...Array(props.ingredients[ingKey])].map((_, i) => {
+            return <BurgerIngredient key={ingKey + i} type={ingKey} />;
+        }); 
+    });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+                {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
--- src/containers/BugerBuilder.js
+++ src/containers/BugerBuilder.js
@@ -1,13 +1,33 @@
+/*
+	=========================================================
+	File: BurgerBuilder.js
+	Date: September 01 2020
+    Description: The purpose of this stateful component is to
+                 build a dynamically created burger.
+	=========================================================
+*/
+
+
 import React, { Component } from 'react';
 import Aux from '../hoc/Aux';
 
 import Burger from '../components/Burger/Burger';
 
 class BugerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            lettuce: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
     render () {
         return (
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </Aux>
         );
