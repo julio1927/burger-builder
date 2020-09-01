9b49c53a3717087223b596b858ee7a477ce703de
julio1927 <jrivas6219@conestogac.on.ca>
2020-09-01 16:43:24 -0400

    Added validation if burger is empty

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -14,11 +14,17 @@ import classes from './Burger.css';
 const burger = (props) => { 
 
     // Mapping object into an array of ingredients. Lets me know how many ingredients I need and what ingredients I need 
-    const transformedIngredients = Object.keys(props.ingredients).map(ingKey => {
+    let transformedIngredients = Object.keys(props.ingredients).map(ingKey => {
         return [...Array(props.ingredients[ingKey])].map((_, i) => {
             return <BurgerIngredient key={ingKey + i} type={ingKey} />;
         }); 
-    });
+    }).reduce((arr,el) => {
+        return arr.concat(el)
+    }, []);
+
+    if (transformedIngredients.length === 0){
+        transformedIngredients = <p> Please start adding ingredients! </p>
+    }
 
     return (
         <div className={classes.Burger}>
