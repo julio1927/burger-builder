885a48aab0a20684f6c4eda82612c562493657b6
julio1927 <jrivas6219@conestogac.on.ca>
2020-09-18 14:03:30 -0400

    added nav items to toolbar with css

---
+++ src/components/Navigation/NavItems/NavItems.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './NavItems.css';
+import NavItem from './NavItem/NavItem';
+
+const navItems = () => (
+    <ul className={classes.NavItems}>
+        <NavItem link="/" active>Burger Builder</NavItem>
+        <NavItem link="/">Checkout</NavItem>
+    </ul>
+);
+
+export default navItems;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavItems from '../NavItems/NavItems';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>Menu</div>
         <Logo />
         <nav>
-            ...
+            <NavItems />
         </nav>
     </header>
 );
