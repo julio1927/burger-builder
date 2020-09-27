52b08d789056771a637c2245bfe3d6369e640337
julio1927 <38108470+julio1927@users.noreply.github.com>
2020-09-27 19:15:32 -0400

    Added menu logo for sidedrawer, also fixed an issue with sidedrawer appearing right away

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,18 +7,24 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 class Layout extends Component {
     
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     }
 
 
     SideDrawerClosedHandler = () => {
         this.setState({showSideDrawer: false});
     }
+
+    SideDrawerToggle = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer}
+        });
+    }
     
     render () {
         return (
             <Aux>
-                <Toolbar />
+                <Toolbar drawerToggleClicked={this.SideDrawerToggle}/>
                 <SideDrawer open={this.state.showSideDrawer} closed={this.SideDrawerClosedHandler}/>
                 <main className={classes.Content}>
                     {this.props.children}
