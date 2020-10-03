9fc4ccfb72ba271aa1a2faf48aa82304012af7e2
julio1927 <38108470+julio1927@users.noreply.github.com>
2020-10-03 02:35:40 -0400

    fixed app preformance, won't re-render Modal component unnecessarily

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) =>{
-    const ingredientSummary = Object.keys(props.ingredients).map(
-        igKey => {
-            return (<li key={igKey}><span style={{ textTransform: 'capitalize' }}>{igKey}
-                        </span>: {props.ingredients[igKey]}</li>);
-        }
-    );
+class OrderSummary extends Component {
 
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
+    // debuggin checking 
+    //componentWillUpdate function 
 
-            <p><strong>Total: ${props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>Cancel</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>Continue</Button>
-        </Aux>
-    );
+    render () {
+        // ingredient summary 
+        const ingredientSummary = Object.keys(this.props.ingredients).map(
+            igKey => {
+                return (<li key={igKey}><span style={{ textTransform: 'capitalize' }}>{igKey}
+                            </span>: {this.props.ingredients[igKey]}</li>);
+            }
+        );
+
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+
+                <p><strong>Total: ${this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>Cancel</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>Continue</Button>
+            </Aux>
+        );
+    }
 };
 
 
-export default orderSummary;
+export default OrderSummary;
